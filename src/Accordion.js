e02fc096eb8d9ea09ea7b2531df331d935b8cbff
Agung Yuliaji <agung.yuliaji@gmail.com>
2017-10-27 17:56:47 +0700

    fix deprecated React.createClass

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Collapsible from 'react-collapsible';
+import createReactClass from 'create-react-class';
 
-var Accordion = React.createClass({
+var Accordion = createReactClass({
 
   //Set validation for prop types
   propTypes: {
